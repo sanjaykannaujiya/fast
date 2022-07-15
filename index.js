e96ff395d18bf6a18ec203bf801969d4130628fc
Sanjay <sballia42@gmail.com>
2022-07-15 21:13:13 +0530

    add new file

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express")
+const bcrypt = require('bcrypt');
+const app = express();
+const ejs=require('ejs')
+const path = require("path")
+const mongoose= require('mongoose')
+const bodyparser=require('body-parser');
+const User = require("./user")
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyparser.json())
+mongoose.connect("mongodb+srv://sanajy:hS4jcFuyzdFLWxiX@cluster0.xxkuqwq.mongodb.net/?retryWrites=true&w=majority",
+{ useUnifiedTopology: true, useNewUrlParser: true }, () =>
+ console.log(" connect"));
+console.log(__dirname ,"public")
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+const port = 3000 
+// signup 
+ app.get('/singup',(req ,res)=>{
+    res.render("home.ejs")
+}) 
+
+ app.post('/singup', async(req ,res)=>{
+    try{
+      let {name ,email ,password} = req.body ; 
+      const hashpassword =  await bcrypt.hash(password ,10)
+      const user = new User({
+        name,
+        email,
+        password:hashpassword,
+         confirm_password:req.body.password
+      })
+      console.log(user)
+      const saveuser = await user.save();
+      console.log(saveuser)
+      res.render("login.ejs")
+    }catch{
+        res.status(500).json(err);
+        console.log(err)
+    }
+}) 
+//login
+app.get("/",(req ,res)=>{
+  res.render("index.html")
+})
+app.get("/login",(req ,res)=>{
+  res.render("login")
+})
+app.post("/login",async(req,res)=>{
+  
+  try{
+    const user = await User.findOne({email: req.body.email});
+   if(!user){
+     res.status(401).json("Wrong credentials");
+   }  
+  const match =  bcrypt.compare(req.body.password, user.password);
+   if (!match) {
+     res.status(401).json("Wrong credentials");
+   }
+   res.render("home.ejs")
+   
+ }catch(err){
+   res.status(500).json(err)
+}
+}
+)
+
+
+app.listen(port ,()=>{console.log("server is start")})
+
